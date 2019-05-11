156329f6a005724022068e9288bf3560756abd29
lukehedger <luke.hedger@gmail.com>
2019-05-11 15:52:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useSprings, animated, interpolate } from "react-spring";
+import { useGesture } from "react-with-gesture";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import kits from "./kits.json";
+import logo from "./kit-pics.png";
+
+const cards = kits
+  .sort(() => 0.5 - Math.random())
+  .slice(0, 10)
+  .map(({ src }) => src);
+
+const to = i => ({
+  x: 0,
+  y: i * -4,
+  scale: 1,
+  rot: -10 + Math.random() * 20,
+  delay: i * 100
+});
+
+const from = i => ({ x: 0, y: -1000, rot: 0, scale: 1.5 });
+
+const trans = (r, s) =>
+  `perspective(1500px) rotateX(30deg) rotateY(${r /
+    10}deg) rotateZ(${r}deg) scale(${s})`;
+
+function Deck() {
+  const [gone] = useState(() => new Set());
+
+  const [springs, set] = useSprings(cards.length, i => ({
+    ...to(i),
+    from: from(i)
+  }));
+
+  const bind = useGesture(
+    ({
+      args: [index],
+      down,
+      delta: [xDelta],
+      distance,
+      direction: [xDir],
+      velocity
+    }) => {
+      const trigger = velocity > 0.2;
+      const dir = xDir < 0 ? -1 : 1;
+
+      if (!down && trigger) gone.add(index);
+      set(i => {
+        if (index !== i) return;
+        const isGone = gone.has(index);
+        const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0;
+        const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0);
+        const scale = down ? 1.1 : 1;
+
+        return {
+          x,
+          rot,
+          scale,
+          delay: undefined,
+          config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 }
+        };
+      });
+
+      if (!down && gone.size === cards.length)
+        setTimeout(() => gone.clear() || set(i => to(i)), 600);
+    }
+  );
+  return springs.map(({ x, y, rot, scale }, i) => (
+    <animated.div
+      key={i}
+      style={{
+        transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`)
+      }}
+    >
+      <animated.div
+        {...bind(i)}
+        style={{
+          transform: interpolate([rot, scale], trans),
+          backgroundImage: `url(${cards[i]})`
+        }}
+      />
+    </animated.div>
+  ));
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <img
+        style={{
+          width: "168px",
+          height: "77px",
+          margin: "0 auto -60px",
+          display: "block"
+        }}
+        alt="Kit Pics"
+        src={logo}
+      />
+
+      <Deck />
+    </>
+  );
+}
